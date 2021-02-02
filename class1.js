4f095d5f52ae44479e077c3a8b605efb846ce381
Syeda Naima Hassan <naima.hassan434@gmail.com>
2021-02-02 16:39:00 +0600

    Let const variable

---
+++ class1.js
@@ -0,0 +1,26 @@
+// console.log('Hello world');
+var ricePrice = 49;
+var onionPrice = 50;
+var potatoPrice = 89;
+var totalPrice = ricePrice + onionPrice + potatoPrice;
+var totalMoney = 250;
+var savings = totalMoney-totalPrice;
+// console.log(savings);
+console.log(100%3);
+var x = 10;
+x++;
+x++;
+x++;
+console.log(x);
+var vara = 30;
+if (vara == 30) {
+  console.log("Rickshaw jabo");
+}
+else {
+  console.log("Hete jabo");
+}
+
+// let const
+
+let fullName = "Naima Hassan"
+console.log(fullName);
