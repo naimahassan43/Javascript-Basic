5ccf4d3d4e4fe90a3a062c906d3e9be6d0972a77
Syeda Naima Hassan <naima.hassan434@gmail.com>
2021-02-10 12:58:12 +0600

    Class-2:Operators

---
+++ class2.js
@@ -0,0 +1,17 @@
+//Operators
+
+//Arithmetic Operator
+// + - * / %
+
+console.log(20 + 10);
+console.log(20 % 10);
+
+// ++ ->increment -- ->Decrement 
+
+let age = 10; 
+
+age--;
+//10^2 
+console.log(age ** 2);
+console.log(10 / 4 * 2);
+console.log(10 * 5 / 2);
